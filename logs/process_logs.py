d8aa07facdd699e3ab1310a660c86bd6bd2bf304
JLSchol <jasper.schol@live.nl>
2025-03-19 14:04:40 +0100

    update processing logs

--- logs/process_logs.py
+++ logs/process_logs.py
@@ -50,12 +50,21 @@ def split_log(log_file):
     activities_file = os.path.join(processed_dir, f"{base_name}_activities.csv")
     trade_history_file = os.path.join(processed_dir, f"{base_name}_tradehistory.txt")
 
-    # Write Sandbox Logs to Text File (or CSV with key-value pairs)
+    # Write Sandbox Logs to Text File 
     if sandbox_logs:
-
         with open(sandbox_file, "w", encoding="utf-8") as file:
             for log in sandbox_logs:
                 file.write(log + "\n")
+
+        with open(sandbox_file, 'r', encoding='utf-8') as file:
+            # Load the content 
+            content = file.read()    
+
+        # Add brackets and comma's for valid json format
+        content = f"[{content.strip().replace('}\n{', '},\n{')}]"
+            
+        with open(sandbox_file, 'w', encoding='utf-8') as file:
+            file.write(content)
 
     # Write Activities Logs to CSV
     if activities_data:
@@ -64,7 +73,7 @@ def split_log(log_file):
             for row in activities_data:
                 writer.writerow(row.split(";"))
 
-    # Write Trade History to CSV
+    # Write Trade History to txt
     if trade_history:
 
         with open(trade_history_file, "w", encoding="utf-8") as file:
@@ -100,6 +109,32 @@ def trade_history_txt_to_csv(input_file):
 
     print(f"Data has been successfully converted to {output_file}")
 
+
+def sandbox_txt_to_csv(input_file):
+    # Step 1: Read the content from the input .txt file
+    with open(input_file, 'r', encoding='utf-8') as file:
+        sand_box_logs = json.loads(file.read())
+
+    # Step 2: Generate the output file name with the same name as input file but with .csv extension
+    output_file = os.path.splitext(input_file)[0] + '.csv'
+    
+    # Step 3: Write the data to the CSV file
+    with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
+        # Define the header for the CSV file
+        fieldnames = ['sandboxLog', 'lambdaLog', 'timestamp']
+        
+        # Create a CSV writer object
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        
+        # Write the header row
+        writer.writeheader()
+        
+        # Write all trade history rows to the CSV file
+        writer.writerows(sand_box_logs)
+
+    print(f"Data has been successfully converted to {output_file}")
+
+
 def split_all_logs(base_dir):
     """Finds and processes all .log files in base_dir and its subdirectories."""
     for root, _, files in os.walk(base_dir):
@@ -117,10 +152,21 @@ def process_all_tradehistory(base_dir):
                 trade_history_txt_to_csv(trade_file_path)
 
 
+def process_all_sandbox(base_dir):
+    """Finds and processes all .log files in base_dir and its subdirectories."""
+    for root, _, files in os.walk(base_dir):
+        for file in files:
+            if file.endswith("sandbox.txt"):
+                trade_file_path = os.path.join(root, file)
+                sandbox_txt_to_csv(trade_file_path)
+
+
 if __name__ == "__main__":
     log_dir = os.path.dirname(__file__)
     split_all_logs(log_dir)
-    print("splitting complete.")
+    print("splitting complete")
     process_all_tradehistory(log_dir)
-    print("processing complete")
+    print("processing tradehistory complete")
+    process_all_sandbox(log_dir)
+    print("processing all sandbox files complete")
 
